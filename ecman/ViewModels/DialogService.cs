790e2fc4f50799481904771f12513f664d14acb1
Lukasz Mocarski <lukasz.mocarski@hotmail.com>
2015-06-11 22:40:18 +0200

    add dialog service class

---
+++ ecman/ViewModels/DialogService.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace ecman.ViewModels
+{
+    public class DialogService
+    {
+       public static async Task<MessageDialogResult> ShowMessage(
+           string message, string title, MessageDialogStyle dialogStyle)
+            {
+                var metroWindow = (Application.Current.MainWindow as MetroWindow);
+                metroWindow.MetroDialogOptions.ColorScheme = MetroDialogColorScheme.Accented;
+                return await metroWindow.ShowMessageAsync(
+                    title, message, dialogStyle, metroWindow.MetroDialogOptions);
+            } 
+    }
+}
